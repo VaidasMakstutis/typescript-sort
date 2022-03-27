223e17ed3eabb2d88300fd1a856aa06fea1c80e3
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-03-27 19:20:19 +0300

    Abstract classes & inheritance

--- build/NumbersCollection.js
+++ build/NumbersCollection.js
@@ -1,9 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NumbersCollection = void 0;
-class NumbersCollection {
+const Sorter_1 = require("./Sorter");
+class NumbersCollection extends Sorter_1.Sorter {
     constructor(data) {
+        super();
         this.data = data;
+    }
+    sort() {
+        const { length } = this;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
+                }
+            }
+        }
     }
     get length() {
         return this.data.length;
